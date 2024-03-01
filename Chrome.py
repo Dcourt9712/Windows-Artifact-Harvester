fc1d0c328b749b7f311b47e7154e0e63eef7ab92
Dcourt9712 <Dcourt9712@github.com>
2024-03-01 10:51:18 -0800

    got chrome history into a csv

--- Chrome.py
+++ Chrome.py
@@ -1,7 +1,7 @@
 
 import os
 import sqlite3
-
+import csv
 
 
 
@@ -10,6 +10,7 @@ Chrome must be closed when running this script otherwise it cannot access the da
 """
 
 def main():
+    columns = ['Urls','Titles','Times']
     name = os.getlogin()
     c =sqlite3.connect('C:\\Users\\'+ name +'\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\History')
     cursor = c.cursor()
@@ -20,6 +21,18 @@ def main():
     cursor.execute('SELECT datetime(last_visit_time/1000000-11644473600, \"unixepoch\") as last_visited, url, title, visit_count FROM urls;')
     times = cursor.fetchall()
     new_times = [i[0] for i in times]
+
+    print(new_times[0])
+  
+
+    with open('ChromeHistory.csv','w',newline='',encoding='utf-8') as myfile:
+        wr = csv.writer(myfile)
+        wr.writerow(("Urls","Titles","Times"))
+        wr.writerows(zip(urls,titles,new_times))
+      
+ 
+      
+    myfile.close()
 
   
 
