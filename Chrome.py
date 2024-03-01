b38e6838c0e7e70f4ce4ba93e9377699d4ea6403
Dcourt9712 <Dcourt9712@github.com>
2024-03-01 12:10:40 -0800

    added visit counts to the spreadsheet

--- Chrome.py
+++ Chrome.py
@@ -21,17 +21,17 @@ def main():
     cursor.execute('SELECT datetime(last_visit_time/1000000-11644473600, \"unixepoch\") as last_visited, url, title, visit_count FROM urls;')
     times = cursor.fetchall()
     new_times = [i[0] for i in times]
+    cursor.execute('SELECT visit_count FROM urls')
+    visit_count = cursor.fetchall()
+
 
   
   
 
     with open('ChromeHistory.csv','w',newline='',encoding='utf-8') as myfile:
         wr = csv.writer(myfile)
-        wr.writerow(("Urls","Titles","Times"))
-        wr.writerows(zip(urls,titles,new_times))
-      
- 
-      
+        wr.writerow(("Urls","Titles","Times","Visit_count"))
+        wr.writerows(zip(urls,titles,new_times,visit_count))
     myfile.close()
 
   
