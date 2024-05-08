70b91a5a537f595b09410e03cdf28695921307ab
Dcourt9712 <143646402+Dcourt9712@users.noreply.github.com>
2024-05-07 22:31:53 -0700

    Add files via upload

--- MRU.py
+++ MRU.py
@@ -3,12 +3,11 @@ import binascii
 import re
 import csv
 
-def main():
-   
+MRU_list = []
 
-    MRU_list = []
+def get_mru(reg_key):
     location = wrg.HKEY_CURRENT_USER
-    Key = wrg.OpenKeyEx(location,r"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32\LastVisitedPidlMRU")
+    Key = wrg.OpenKeyEx(location,reg_key)
     for x in range(0,1024):
         try:
             value = wrg.QueryValueEx(Key,str(x))
@@ -19,10 +18,21 @@ def main():
     if Key:
         wrg.CloseKey(Key)
 
-   
-    for items in range(0,len(MRU_list)):
-        print(MRU_list[items])
-
+def main():
+    location = wrg.HKEY_CURRENT_USER
+    reg_key =r"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32\OpenSavePidlMRU"
+    key = wrg.OpenKeyEx(location,reg_key,0,wrg.KEY_READ)
+  
+    for i in range(200):
+        try:
+            subkey = wrg.EnumKey(key, i)
+            val =( str(reg_key + '\\' +str(subkey)))
+            get_mru(val)
+        except:
+            
+            break
+    
+    get_mru(r"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32\LastVisitedPidlMRU")
     header = ['item']
     with open('MRU.csv','w',newline='',encoding='utf-8') as myfile:
         wr = csv.writer(myfile)
