e3e752edbc84bea642b5bf5f2ab3ad8c89b0a840
Dcourt9712 <143646402+Dcourt9712@users.noreply.github.com>
2024-05-09 11:28:08 -0700

    Add files via upload

--- Networks.py
+++ Networks.py
@@ -2,7 +2,11 @@ import winreg as wrg
 import binascii
 import re
 import csv
+import chardet
 
+import struct
+from binascii import unhexlify
+from datetime import datetime, timedelta
 networks_list = []
 
 def get_previous_networks(g_key):
@@ -12,14 +16,72 @@ def get_previous_networks(g_key):
     Key = wrg.OpenKeyEx(location,g_key)
 
     network_name = ""
+    date_created = ""
   
     try:
         network_name = wrg.QueryValueEx(Key,"ProfileName")
         net = net + str(network_name[0])
     except WindowsError as e:
         pass
-    
+
+    try:
+        
+        date_created = wrg.QueryValueEx(Key,"DateCreated")
+        raw = date_created[0].hex()
+      
+        year = int(str(raw[2:4] + raw[0:2]),16)
+        month = int(str(raw[7:8] + raw[5:6]),16)
+        day_number =int(str(raw[15:16] + raw[13:14]),16)
+
+        hour = str(int(str(raw[19:20] + raw[17:18]),16))
+        if(len(hour) < 2):
+            hour = "0" + str(hour)
+        
+        minute = str(int(str(raw[23:24] + raw[21:22]),16))
+        if(len(minute) < 2):
+            minute = "0" + str(minute)
+
+        seconds = str(int(str(raw[27:28] + raw[25:26]),16))
+
+        if(len(seconds) < 2):
+            seconds = "0" + str(seconds)
+
+        date = str(year) + "-" + str(month) + "-" + str(day_number) + " "+ str(hour) + ":" + str(minute) + ":" + str(seconds)
+
+        net = net + " ," + date
+    except WindowsError as e:
+        pass
+
+
+    try:
+        
+        date_last_connected = wrg.QueryValueEx(Key,"DateLastConnected")
+        raw = date_last_connected[0].hex()
+      
+        year = int(str(raw[2:4] + raw[0:2]),16)
+        month = int(str(raw[7:8] + raw[5:6]),16)
+        day_number =int(str(raw[15:16] + raw[13:14]),16)
+
+        hour = str(int(str(raw[19:20] + raw[17:18]),16))
+        if(len(hour) < 2):
+            hour = "0" + str(hour)
+        
+        minute = str(int(str(raw[23:24] + raw[21:22]),16))
+        if(len(minute) < 2):
+            minute = "0" + str(minute)
+
+        seconds = str(int(str(raw[27:28] + raw[25:26]),16))
+
+        if(len(seconds) < 2):
+            seconds = "0" + str(seconds)
+
+        date = str(year) + "-" + str(month) + "-" + str(day_number) + " "+ str(hour) + ":" + str(minute) + ":" + str(seconds)
+
+        net = net + " ," + date
+    except WindowsError as e:
+        pass
    
+    
     networks_list.append(net.split(','))
 
     if Key:
@@ -46,8 +108,8 @@ for i in range(0,len(key_list)):
 
 
 with open('PreviousNetworks.csv','w',newline='',encoding='utf-8') as myfile:
-    wr = csv.writer(myfile)
-    wr.writerow((["Names"]))
+    wr = csv.writer(myfile, escapechar = '\\')
+    wr.writerow((["Names","DateCreated", "DateLastConnected" ]))
     for row in range(0,len(networks_list)):
         wr.writerow(networks_list[row])
   
